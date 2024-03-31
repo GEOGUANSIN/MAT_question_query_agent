5bc1dff6df18b97313e5ad1d0fb80847e322b049
GEOGUANSIN <garguan2001@outlook.com>
2024-03-31 20:03:57 +0800

    ollama

--- text.py
+++ text.py
@@ -1,4 +1,18 @@
 # for i in range(4):
 #     print(i)
 
-print([i for i in range(20, 24+1)])
+# print([i for i in range(20, 24+1)])
+
+from langchain_community.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+# Print the answer
+llm = Ollama(model = 'llama2', callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
+conversation_history = ""
+while True:
+    input_ = input('\ninput:')
+    if input_ == 'exit':
+        break
+    answer = llm.invoke(f'The following is a conversation between a human and an AI. The conversation history is: {conversation_history}\nHuman: {input_}\nAI:')
+    conversation_history = conversation_history + f'AI:{answer} Human:{input_}'
